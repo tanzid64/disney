d55b278b4579a2cdbd3c607e91657ff3d607c23d
tanzid64 <tanzid@inbox.ru>
2024-03-22 03:11:27 +0600

    Genre Movie List loop limit removed

--- src/Components/Genre/GenreMovieList.jsx
+++ src/Components/Genre/GenreMovieList.jsx
@@ -1,19 +1,15 @@
 import MovieList from "./MovieList";
 import { genere } from "./data";
 
-
 const GenreMovieList = () => {
   return (
     <div className="">
-      {genere.map(
-        (item, index) =>
-          index < 4 && (
-            <div key={item.id} className="p-8 md:px-16">
-              <h2 className="text-[20px] font-bold">{item.name}</h2>
-              <MovieList key={item.id} genereId={item.id} index_={index} />
-            </div>
-          )
-      )}
+      {genere.map((item, index) => (
+        <div key={item.id} className="p-8 md:px-16">
+          <h2 className="text-[20px] font-bold">{item.name}</h2>
+          <MovieList key={item.id} genereId={item.id} index_={index} />
+        </div>
+      ))}
     </div>
   );
 };
